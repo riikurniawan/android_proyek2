42e488e5176f816c98b96422bcbd1c29196fd26c
Ari Kurniawan <arikurnia2601@gmail.com>
2022-08-10 13:19:40 +0700

    initial commit

---
+++ app/src/main/java/com/rii/proyek3/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.rii.proyek3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code here
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code here
+            }
+        });
+    }
+}
