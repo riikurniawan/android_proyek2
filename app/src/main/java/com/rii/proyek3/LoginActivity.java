d989a09e37c61eb5abd09f3cbce0b038609bc3ba
Ari Kurniawan <arikurnia2601@gmail.com>
2022-08-10 16:38:16 +0700

    menambahkan logic pada login

--- app/src/main/java/com/rii/proyek3/LoginActivity.java
+++ app/src/main/java/com/rii/proyek3/LoginActivity.java
@@ -6,9 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
 
+    final String FILENAME = "login";
+
+    EditText editUsername, editPassword;
     Button btnLogin, btnRegister;
 
     @Override
@@ -18,13 +29,20 @@ public class LoginActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Login");
 
+        editUsername = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.editPassword);
+
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //code here
+                if(editUsername.getText().toString().isEmpty() || editPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "username dan password wajib diisi!", Toast.LENGTH_SHORT).show();
+                } else {
+                    login();
+                }
             }
         });
 
@@ -35,5 +53,55 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    void login() {
+        File sdcard = getFilesDir();
+        File file = new File(sdcard, editUsername.getText().toString());
+
+        if(file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while(line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException ioe) {
+                System.out.println("Error: " + ioe.getMessage());
+            }
+
+            String data = text.toString();
+            String[] dataUser = data.split(";");
+            
+            if(dataUser[1].equals(editPassword.getText().toString())) {
+                simpanFileLogin();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(this, "password salah!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "user tidak ditemukan!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void simpanFileLogin() {
+        String isiFIle = editUsername.getText().toString() + ";" + editPassword.getText().toString();
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, false);
+            outputStream.write(isiFIle.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
